aedf928b4dceceb4cf739ad37267dfc142c30aa9
nihad1213 <nihad.nemetli@gmail.com>
2025-11-20 21:22:27 +0400

    add: getLine function added in header file

--- src/include/custom_io.h
+++ src/include/custom_io.h
@@ -5,9 +5,11 @@
 #ifndef CUSTOM_IO_H
 #define CUSTOM_IO_H
 
-#define ERROR  -1;
-#define SUCCESS 0;
-#define BUFSIZE 1024;
+#include <stdio.h>
+
+#define ERROR  -1
+#define SUCCESS 0
+#define BUFSIZE 1024
 
 // Function definitions
 int printSingleCharacter(char chr);
@@ -15,5 +17,6 @@ int printString(const char *str);
 int printError(const char *str);
 int printWarning(const char *str);
 int printSuccess(const char *str);
+ssize_t getLine(char **line, size_t *n, FILE *stream);
 
 #endif
