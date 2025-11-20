7b189c569b1874c947b6461c36912238b2c97fa7
nihad1213 <nihad.nemetli@gmail.com>
2025-11-20 22:02:27 +0400

    main.c first logic implementation created

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
 #include "include/custom_io.h"
 
 int main() {
-    
+    char input[BUFSIZE];
+
+    while (1) {
+        printSingleCharacter('$');
+        int len = getLine(input, BUFSIZE);
+
+        if (len == -1) {
+            printError("EOF received. Exiting...\n");
+            break;
+        }
+
+        if (len == 0) continue;
+
+        // Commands will start in here
+
+    }
     
 
     return 0;
